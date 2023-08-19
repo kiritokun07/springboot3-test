9dac81f16444120941826f0a492ee93ad600f2e6
kirito <504831261@qq.com>
2023-08-19 18:02:32 +0800

    ~

--- src/main/java/com/kirito/test/controller/YUserController.java
+++ src/main/java/com/kirito/test/controller/YUserController.java
@@ -1,13 +1,14 @@
 package com.kirito.test.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.kirito.test.domain.YUser;
 import com.kirito.test.service.IYUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * <p>
@@ -25,8 +26,10 @@ public class YUserController {
     private final IYUserService iyUserService;
 
     @GetMapping("test1")
-    public List<YUser> test1() {
-        List<YUser> list = iyUserService.list();
-        return list;
+    public IPage<YUser> test1() {
+        IPage<YUser> page = new Page<>(1, 10);
+        LambdaQueryChainWrapper<YUser> queryChainWrapper = iyUserService.lambdaQuery();
+        queryChainWrapper.page(page);
+        return page;
     }
 }
